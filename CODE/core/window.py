e3bb9704db5810bbe92e6720ce8f8c8013616bf7
Kenny Bravo Rodriguez <kenny.bravo85@gmail.com>
2023-01-30 14:52:42 +0100

    CorrA Win

--- CODE/core/window.py
+++ CODE/core/window.py
@@ -190,6 +190,7 @@ class BaseWindowResult(BaseWindow):
         self.rDate  = getattr(self, 'rDate',  [])
         self.rDateC = getattr(self, 'rDateC', '')
         self.rData  = getattr(self, 'rData',  cMethod.BaseAnalysis())
+        self.rDataC = getattr(self, 'rDataC', pd.DataFrame())
         self.rDf    = getattr(self, 'rDf',    pd.DataFrame())
         self.rObj: resFile.UMSAPFile
         #------------------------------>
@@ -249,8 +250,9 @@ class BaseWindowResult(BaseWindow):
         """
         #region ---------------------------------------------> Nothing to Plot
         if len(self.rData.date) < 1:
-            msg = (f'All {self.cSection} in file {self.rObj.rFileP.name} are '
-                f'corrupted or were not found.')
+            msg = (f'All entries for {self.cSection} in file '
+                   f'{self.rObj.rFileP.name} are corrupted or were not found.')
+            Notification('errorU', msg=msg)
             raise ValueError(msg)
         #endregion ------------------------------------------> Nothing to Plot
 
@@ -310,11 +312,10 @@ class BaseWindowResult(BaseWindow):
             p   = Path(dlg.GetPath())
             tDF = df
             if tDF is None:
-                data = getattr(self.rData, self.rDateC)
-                tDF = data.df if df is None else df
+                tDF = self.rDataC.df if df is None else df
             #------------------------------> Export
             try:
-                cFile.WriteDF2CSV(p, tDF)
+                cFile.WriteDF2CSV(p, tDF)                                       # type: ignore
             except Exception as e:
                 Notification(
                     'errorF',
