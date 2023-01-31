14400e5542e350bd148a0063e2b7fa434b739352
Kenny Bravo Rodriguez <kenny.bravo85@gmail.com>
2023-01-31 12:29:02 +0100

    Gen Code Imp dataprep

--- CODE/dataprep/method.py
+++ CODE/dataprep/method.py
@@ -36,7 +36,7 @@ LIT_Imp  = Literal['', 'None', 'Normal Distribution']
 class UserData(cMethod.BaseUserData):
     """Representation of the input data for the Correlation Analysis Pane"""
     #region ---------------------------------------------------------> Options
-    dO:list = field(default_factory=lambda:
+    dO:list = field(default_factory=lambda:                                     # Attr printed to UMSAP file
         ['iFileN', 'ID', 'cero', 'tran', 'norm', 'imp', 'shift',
          'width', 'ocResCtrlFlat', 'dfColumnR', 'dfColumnF', 'dfResCtrlFlat',
         ])
